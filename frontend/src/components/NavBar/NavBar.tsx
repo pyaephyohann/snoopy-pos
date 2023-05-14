c6a2ba379c0adb4a3786e632258d1aae50b0f9c6
Pyae Phyo Han <pyaephyohan2200@gmail.com>
2023-05-15 00:36:08 +0630

    feat: added sidebar-drawer

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -6,15 +6,30 @@ import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-import Switch from "@mui/material/Switch";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormGroup from "@mui/material/FormGroup";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
+import { useState } from "react";
+import LunchDiningIcon from "@mui/icons-material/LunchDining";
+import LocalDiningIcon from "@mui/icons-material/LocalDining";
+import SettingsIcon from "@mui/icons-material/Settings";
+import LocalMallIcon from "@mui/icons-material/LocalMall";
+import ClassIcon from "@mui/icons-material/Class";
+import CategoryIcon from "@mui/icons-material/Category";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import {
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 
 const NavBar = () => {
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [open, setOpen] = useState(false);
+  const [auth, setAuth] = useState(true);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -23,6 +38,62 @@ const NavBar = () => {
   const handleClose = () => {
     setAnchorEl(null);
   };
+
+  const sidebarMenuItems = [
+    { id: 1, label: "Orders", icon: <LocalMallIcon />, route: "/orders" },
+    { id: 2, label: "Menus", icon: <LocalDiningIcon />, route: "/menus" },
+    {
+      id: 3,
+      label: "Menu Categories",
+      icon: <CategoryIcon />,
+      route: "/menu-categories",
+    },
+    { id: 4, label: "Addons", icon: <LunchDiningIcon />, route: "/addons" },
+    {
+      id: 5,
+      label: "Addon Categories",
+      icon: <ClassIcon />,
+      route: "/addon-categories",
+    },
+    {
+      id: 6,
+      label: "Locations",
+      icon: <LocationOnIcon />,
+      route: "/locations",
+    },
+    { id: 7, label: "Settings", icon: <SettingsIcon />, route: "/settings" },
+  ];
+
+  const renderDrawer = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={() => setOpen(false)}
+      onKeyDown={() => setOpen(false)}
+    >
+      <List>
+        {sidebarMenuItems.slice(0, 6).map((menuItem) => (
+          <ListItem key={menuItem.id} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>{menuItem.icon}</ListItemIcon>
+              <ListItemText primary={menuItem.label} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {sidebarMenuItems.slice(-1).map((menuItem) => (
+          <ListItem key={menuItem.id} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>{menuItem.icon}</ListItemIcon>
+              <ListItemText primary={menuItem.label} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -34,6 +105,7 @@ const NavBar = () => {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={() => setOpen(true)}
           >
             <MenuIcon />
           </IconButton>
@@ -74,6 +146,11 @@ const NavBar = () => {
           )}
         </Toolbar>
       </AppBar>
+      <Box>
+        <Drawer open={open} onClose={() => setOpen(false)}>
+          {renderDrawer()}
+        </Drawer>
+      </Box>
     </Box>
   );
 };
