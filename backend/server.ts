06f2a739b67e44104c54c68a2870bab8ead48b64
Pyae Phyo Han <pyaephyohan2200@gmail.com>
2023-05-14 16:52:35 +0630

    feat: encrypt password with bcrypt

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { db } from "./src/db/db";
+import bcrypt from "bcrypt";
 
 const app = express();
 
@@ -10,9 +11,10 @@ app.use(express.json());
 app.post("/auth/register", async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) return res.sendStatus(400);
+  const hashedPassword = await bcrypt.hash(password, 10);
   const text =
     "INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *";
-  const values = [name, email, password];
+  const values = [name, email, hashedPassword];
   try {
     const userResult = await db.query(text, values);
     const user = userResult.rows[0];
@@ -21,6 +23,18 @@ app.post("/auth/register", async (req: Request, res: Response) => {
   } catch (error) {
     res.sendStatus(500);
   }
+});
+
+app.post("/auth/login", async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  if (!email || !password) return res.sendStatus(400);
+  const userResult = await db.query("select * from users where email = $1", [
+    email,
+  ]);
+  const user = userResult.rows[0];
+  const hashedPassword = user.password;
+  const isCorrectPassword = await bcrypt.compare(password, hashedPassword);
+  return isCorrectPassword ? res.sendStatus(200) : res.sendStatus(401);
 });
 
 app.listen(5000, () => {
---
+++ frontend/src/components/Login/Login.tsx
@@ -0,0 +1,59 @@
+import { Box, Button, TextField } from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState({ email: "", password: "" });
+  const login = async () => {
+    const response = await fetch("http://localhost:5000/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    if (response.ok) {
+      navigate("/home");
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "20rem",
+        m: "0 auto",
+        mt: "5rem",
+        padding: "2rem",
+        border: "3px solid #C9A7EB",
+      }}
+    >
+      <TextField
+        onChange={(event) => setUser({ ...user, email: event.target.value })}
+        label="Email"
+        variant="outlined"
+      />
+      <TextField
+        onChange={(event) => setUser({ ...user, password: event.target.value })}
+        sx={{ my: 3 }}
+        type="password"
+        label="Password"
+        variant="outlined"
+      />
+      <Button
+        sx={{
+          backgroundColor: "#C9A7EB",
+          ":hover": { backgroundColor: "#F7D060" },
+        }}
+        variant="contained"
+        onClick={login}
+      >
+        Login
+      </Button>
+    </Box>
+  );
+};
+
+export default Login;
